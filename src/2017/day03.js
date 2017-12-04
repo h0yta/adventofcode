5f9d09b9421b56a07bdaf11c48bc475cbd45def3
h0yta <oscar@vettig.se>
2017-12-04 19:41:39 +0100

    2017-03

--- src/2017/day03.js
+++ src/2017/day03.js
@@ -1,15 +1,30 @@
 const utils = require('../util/fileUtil');
 
-let sumOne = 0;
-let sumTwo = 0;
-
-const run = function () {
-  let input = utils.getInput('day03', '');
-
-  console.log(input);
-
-  console.log('sumOne', sumOne);
-  console.log('sumTwo', sumTwo);
+const runOne = function (input) {
+  let pos = findPos(0, 0, input);
+  let noPerLine = parseInt((pos.lap * 2 + 1) / 2);
+  let posOnLine = noPerLine <= 0 ? 0 : (pos.lapLastPos % noPerLine) - 1;
+  if (posOnLine < 0) posOnLine = posOnLine * -1;
+  pos.lapLastPos = posOnLine === 0 ? 0 : (noPerLine - posOnLine);
+  let steps = pos.lapLastPos + pos.lap;
+  console.log(input + ' carries ' + steps + ' steps');
 }
 
-run();
+const findPos = function (y, sum, input) {
+  if ((sum + (y * 8) + 1) >= input) {
+    return {
+      'lap': y,
+      'lapLastPos': (input - sum)
+    };
+  }
+  let newSum = sum + (y * 8) + 1;
+  return findPos(++y, newSum, input);
+}
+
+exports.run = function () {
+  runOne(1);
+  runOne(12);
+  runOne(23);
+  runOne(1024);
+  runOne(312051);
+}
