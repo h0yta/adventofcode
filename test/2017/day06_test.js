ef1e978811be40acaf1baa0fb3d36684b3000b0f
h0yta <oscar@vettig.se>
2017-12-06 06:14:58 +0100

    2017-06

--- test/2017/day06_test.js
+++ test/2017/day06_test.js
@@ -5,9 +5,9 @@ describe('Day 06', function () {
 
   describe('firstRun', function () {
 
-    it('should return 0', function () {
-      let input = [];
-      assert.equal(sut.runOne(input), 0);
+    it('should return 5', function () {
+      let input = [0, 2, 7, 0];
+      assert.equal(sut.runOne(input), 5);
     });
 
   });
