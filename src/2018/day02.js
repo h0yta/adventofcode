9a29f729ab9b31905d50e23313d2f2faf22e2f9c
h0yta <oscar@vettig.se>
2018-12-02 08:15:53 +0100

    Solution for day 2

--- src/2018/day02.js
+++ src/2018/day02.js
@@ -1,11 +1,37 @@
 const utils = require('../util/fileUtil');
 
 const firstStar = function (input) {
-  return 0;
+  let exactlyTwice = 0;
+  let exactlyThree = 0;
+  input.forEach(elem => {
+    let occurences = elem
+      .split("")
+      .filter(function (item, pos) {
+        return elem.indexOf(item) === pos;
+      }).map(x => {
+        return parseInt(elem.split(x).length - 1);
+      });
+
+    exactlyTwice += occurences.filter(x => x === 2).length > 0 ? 1 : 0;
+    exactlyThree += occurences.filter(x => x === 3).length > 0 ? 1 : 0;
+  });
+
+  return exactlyTwice * exactlyThree;
 }
 
 const secondStar = function (input) {
-  return 0;
+  for (let i = 0; i < input.length - 1; i++) {
+    let arrayI = [...input[i]];
+
+    for (let j = i + 1; j < input.length; j++) {
+      let arrayJ = [...input[j]];
+      let diff = arrayI.reduce((p, c, i) => p + (c === arrayJ[i] ? 0 : 1), 0)
+
+      if (diff === 1) {
+        return arrayI.filter((c, i) => c === arrayJ[i]).join('');
+      }
+    }
+  }
 }
 
 exports.run = function () {
