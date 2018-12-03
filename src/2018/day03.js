74b864bec5c4919119ce15bad544d68e80eef376
h0yta <oscar@vettig.se>
2018-12-03 06:24:26 +0100

    Day 3

--- src/2018/day03.js
+++ src/2018/day03.js
@@ -8,6 +8,22 @@ const secondStar = function (input) {
   return 0;
 }
 
+const parseInput = function (row) {
+  // #1 @ 1,3: 4x4
+  let myChildRegexp = /^#.*\s@\s(.*),(.*):\s(.*)x(.*)$/;
+  let match = myChildRegexp.exec(row);
+  if (match === null) {
+    console.log('Found no match for', row);
+  } else {
+    return {
+      "startX": match[1].trim(),
+      "startY": match[2].trim(),
+      "lengthX": match[3].trim(),
+      "lengthY": match[4].trim()
+    }
+  }
+}
+
 exports.run = function () {
   let input = utils.getInput('2018', 'day03', '\n');
   console.log('Answer for first star', firstStar(input));
@@ -16,3 +32,4 @@ exports.run = function () {
 
 exports.runOne = firstStar;
 exports.runTwo = secondStar;
+exports.parse = parseInput;
--- test/2018/day03_test.js
+++ test/2018/day03_test.js
@@ -3,11 +3,26 @@ const assert = require('assert');
 
 describe('Day 03', function () {
 
+  describe('parse', function () {
+
+    it('should return {}}', function () {
+      let input = '#1 @ 1,3: 4x4';
+      let result = sut.parse(input);
+      assert.equal(result.startX, 1);
+      assert.equal(result.startY, 3);
+      assert.equal(result.lengthX, 4);
+      assert.equal(result.lengthY, 4);
+    });
+
+  });
+
   describe('firstStar', function () {
 
     it('should return 0', function () {
-      let input = [];
-      assert.equal(sut.runOne(input), 0);
+      let input = ['#1 @ 1,3: 4x4',
+        '#2 @ 3,1: 4x4',
+        '#3 @ 5,5: 2x2'];
+      assert.equal(sut.runOne(input), 4);
     });
 
   });
