c669d4b10af578e606538d7faa5a936ef035c0dd
h0yta <oscar@vettig.se>
2018-12-09 15:51:35 +0100

    Day 09

--- src/2018/day09.js
+++ src/2018/day09.js
@@ -1,17 +1,81 @@
 const utils = require('../util/fileUtil');
 
 const firstStar = function (input) {
-  return 0;
+  [input, noPlayers, lastMarble] = parseInput(input);
+  return runGameArray(parseInt(noPlayers), parseInt(lastMarble));
+}
+
+const runGameArray = function (noPlayers, lastMarble) {
+  let players = new Array(noPlayers).fill(0);
+  for (let circle = [0], marble = 1, currentMarble = 1, player = 0; marble <= lastMarble; marble++ , player++) {
+    player = player % noPlayers;
+    let index = ((currentMarble + 1) % circle.length) + 1;
+
+    if (marble % 23 === 0) {
+      index = ((currentMarble + circle.length - 7) % circle.length);
+      players[player] += marble;
+      players[player] += circle[index];
+      circle.splice(index, 1);
+    } else {
+      circle.splice(index, 0, marble);
+    }
+
+    currentMarble = index;
+  }
+
+  return Math.max(...players);
 }
 
 const secondStar = function (input) {
-  return 0;
+  [input, noPlayers, lastMarble] = parseInput(input);
+  return runGameLinkedList(parseInt(noPlayers), parseInt(lastMarble) * 100);
+}
+
+const runGameLinkedList = function (noPlayers, lastMarble) {
+  let players = new Array(noPlayers).fill(0);
+  let currentMarble = {
+    index: 0,
+  };
+  currentMarble.next = currentMarble;
+  currentMarble.prev = currentMarble;
+
+  for (let marble = 1, player = 0; marble <= lastMarble; marble++ , player++) {
+    player = player % noPlayers;
+
+    if (marble % 23 === 0) {
+      players[player] += marble;
+      currentMarble = currentMarble.prev.prev.prev.prev.prev.prev;
+      players[player] += currentMarble.prev.index;
+      currentMarble.prev.prev.next = currentMarble;
+      currentMarble.prev = currentMarble.prev.prev;
+    } else {
+      currentMarble = addNext(marble, currentMarble.next);
+    }
+  }
+
+  return Math.max(...players);
+}
+
+const addNext = function (index, marble) {
+  const next = {
+    index,
+    prev: marble,
+    next: marble.next,
+  }
+
+  marble.next.prev = next;
+  marble.next = next;
+  return next;
+}
+
+const parseInput = function (input) {
+  return input.match(/(\d+) players; last marble is worth (\d+) points/);
 }
 
 exports.run = function () {
-  let input = utils.getInput('2018', 'day09', '');
-  console.log('Answer for first star', firstStar(input));
-  console.log('Answer for second star', secondStar(input));
+  let input = utils.getInput('2018', 'day09', '\n');
+  console.log('Answer for first star', firstStar(input[0]));
+  console.log('Answer for second star', secondStar(input[0]));
 }
 
 exports.runOne = firstStar;
--- test/2018/day09_test.js
+++ test/2018/day09_test.js
@@ -5,18 +5,44 @@ describe('2018 - Day 09', function () {
 
   describe('firstStar', function () {
 
-    it('should return 0', function () {
-      let input = [''];
-      assert.equal(sut.runOne(input), 0);
+    it('should return 9 players with last marble worth 25', function () {
+      let input = '9 players; last marble is worth 25 points';
+      assert.equal(sut.runOne(input), 32);
+    });
+
+    it('should return 10 players with last marble worth 1618', function () {
+      let input = '10 players; last marble is worth 1618 points';
+      assert.equal(sut.runOne(input), 8317);
+    });
+
+    it('should return 13 players with last marble worth 7999', function () {
+      let input = '13 players; last marble is worth 7999 points';
+      assert.equal(sut.runOne(input), 146373);
+    });
+
+    it('should return 17 players with last marble worth 1104', function () {
+      let input = '17 players; last marble is worth 1104 points';
+      assert.equal(sut.runOne(input), 2764);
+    });
+
+    it('should return 21 players with last marble worth 6111', function () {
+      let input = '21 players; last marble is worth 6111 points';
+      assert.equal(sut.runOne(input), 54718);
+    });
+
+    it('should return 30 players with last marble worth 5807', function () {
+      let input = '30 players; last marble is worth 5807 points';
+      assert.equal(sut.runOne(input), 37305);
     });
 
   });
 
   describe('secondStar', function () {
 
-    it('should return 0', function () {
-      let input = [''];
-      assert.equal(sut.runTwo(input), 0);
+    it('should return 9 players with last marble worth 25', function () {
+      let input1 = '9 players; last marble is worth 2500 points';
+      let input2 = '9 players; last marble is worth 25 points';
+      assert.equal(sut.runTwo(input2), sut.runOne(input1));
     });
 
   });
