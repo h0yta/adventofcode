cc035538958bc42e5b53093419c6957ac48fb79f
h0yta <oscar@vettig.se>
2018-12-11 06:55:25 +0100

    Day 11

--- src/2018/day11.js
+++ src/2018/day11.js
@@ -1,7 +1,49 @@
 const utils = require('../util/fileUtil');
 
 const firstStar = function (input) {
-  return 0;
+  let grid = Array(300)
+    .fill(0)
+    .map(() => Array(300).fill(0));
+
+  for (let x = 0; x < 300; x++) {
+    for (let y = 0; y < 300; y++) {
+      grid[y][x] = calculateFuelLevel(x, y, input);
+    }
+  }
+
+  let result = {
+    'x': 0,
+    'y': 0,
+    'level': 0
+  };
+  for (let x = 0; x < 300 - 3; x++) {
+    for (let y = 0; y < 300 - 3; y++) {
+      let level = calculateGridLevel(grid, x, y);
+      if (level > result.level) {
+        result.x = x;
+        result.y = y;
+        result.level = level;
+      }
+    }
+  }
+
+  console.log(result);
+
+}
+
+const calculateFuelLevel = function (x, y, serial) {
+  let rackId = x + 10;
+  return Math.floor(((rackId * y + serial) * rackId) / 100 % 10) - 5;
+}
+
+const calculateGridLevel = function (grid, xStart, yStart) {
+  let sum = 0;
+  for (let x = xStart; x < xStart + 3; x++) {
+    for (let y = yStart; y < yStart + 3; y++) {
+      sum += grid[y][x];
+    }
+  }
+  return sum;
 }
 
 const secondStar = function (input) {
@@ -10,9 +52,10 @@ const secondStar = function (input) {
 
 exports.run = function () {
   let input = utils.getInput('2018', 'day11', '\n');
-  console.log('Answer for first star', firstStar(input));
-  console.log('Answer for second star', secondStar(input));
+  console.log('Answer for first star', firstStar(input[0]));
+  console.log('Answer for second star', secondStar(input[0]));
 }
 
 exports.runOne = firstStar;
 exports.runTwo = secondStar;
+exports.calculateFuelLevel = calculateFuelLevel;
--- test/2018/day11_test.js
+++ test/2018/day11_test.js
@@ -3,11 +3,35 @@ const assert = require('assert');
 
 describe('2018 - Day 11', function () {
 
+  describe('calculateFuelLevel', function () {
+
+    it('calculateFuelLevel should return 4', function () {
+      let input = [];
+      assert.equal(sut.calculateFuelLevel(3, 5, 8), 4);
+    });
+
+    it('calculateFuelLevel should return -5', function () {
+      let input = [];
+      assert.equal(sut.calculateFuelLevel(122, 79, 57), -5);
+    });
+
+    it('calculateFuelLevel should return 0', function () {
+      let input = [];
+      assert.equal(sut.calculateFuelLevel(217, 196, 39), 0);
+    });
+
+    it('calculateFuelLevel should return 4', function () {
+      let input = [];
+      assert.equal(sut.calculateFuelLevel(101, 153, 71), 4);
+    });
+
+  });
+
   describe('firstStar', function () {
 
-    it('should return 0', function () {
-      let input = [];
-      assert.equal(sut.runOne(input), 0);
+    it('should return 33,45', function () {
+      let input = [18];
+      assert.equal(sut.runOne(input), '33,45');
     });
 
   });
