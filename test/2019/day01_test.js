4f1c73f0d4da3fc191a34ee8d2ad767b5f7f6f89
hoyta <freakshow7>
2019-11-30 11:34:32 +0100

    2019 HYPE!

---
+++ test/2019/day01_test.js
@@ -0,0 +1,23 @@
+const sut = require('../../src/2019/day01');
+const assert = require('assert');
+
+describe('2019 - Day 01', function () {
+
+  describe('firstStar', function () {
+
+    it('', function () {
+
+    });
+
+  });
+
+  describe('secondStar', function () {
+
+    it('', function () {
+
+    });
+
+  });
+
+});
+
