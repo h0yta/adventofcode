757265909c0e64cbeb595691d414b4985d4d5c8e
Oscar Törnroth <oscar.tornroth@jordbruksverket.se>
2019-12-03 16:26:15 +0100

    Day 03 - part 1

--- src/2019/day03.js
+++ src/2019/day03.js
@@ -1,11 +1,61 @@
 const utils = require('../util/fileUtil');
 
+let x;
+let y;
+let path = new Array();
+let encounters = new Array();
 const firstStar = function (input) {
-  return 0;
+  path = new Array();
+  input.forEach(wire => {
+    x = 0;
+    y = 0;
+    encounters = new Array();
+    let instructions = wire.split(",");
+    instructions.forEach(instruction => {
+      movePosition(instruction);
+    });
+  });
+
+  return Math.min(...encounters)
 }
 
 const secondStar = function (input) {
   return 0;
+}
+
+const movePosition = (instruction) => {
+  let direction = instruction.substring(0, 1);
+  let distance = instruction.substring(1, instruction.length);
+
+  for (i = 0; i < distance; i++) {
+    switch (direction) {
+      case 'R':
+        x++;
+        break;
+      case 'U':
+        y++;
+        break;
+      case 'L':
+        x--;
+        break;
+      case 'D':
+        y--;
+        break;
+    }
+
+    let key = createPositionKey(x, y);
+    if (path.includes(key)) {
+      
+      let distanceFromOrigo = Math.abs(x) + Math.abs(y);
+      encounters.push(distanceFromOrigo);
+    } else {
+      path.push(key);
+    }
+  }
+}
+
+const createPositionKey = (x, y) => {
+  return x + '#' + y;
 }
 
 exports.run = function () {
