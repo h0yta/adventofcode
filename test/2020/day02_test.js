6127c551c8f995a5e4cc4a0173a5e52f2b38a4b6
hoyta <freakshow7>
2020-12-02 20:08:11 +0100

    Jingle bell, jingle bell, jingle bell rock
    Jingle bells chime in jingle bell time
    Dancin' and prancin' in Jingle Bell Square
    In the frosty air

--- test/2020/day02_test.js
+++ test/2020/day02_test.js
@@ -7,7 +7,7 @@ describe('2020 - Day 02', function () {
 
     let passwords = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc'];
 
-    it('should ...', function () {
+    it('should find number of valid passwords', function () {
       assert.strictEqual(sut.runOne(passwords), 2);
     });
 
@@ -15,7 +15,7 @@ describe('2020 - Day 02', function () {
 
   describe('secondStar', function () {
 
-    it('should ...', function () {
+    it('should find number of valid passwords again', function () {
       assert.strictEqual(sut.runTwo(['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']), 1);
     });
 
