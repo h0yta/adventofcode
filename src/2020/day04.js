e7b5d4058720c24ac7d80c69b61d970eae676093
h0yta <oscar@vettig.se>
2020-12-04 07:20:10 +0100

    The holly and the ivy
    When they are both full grown
    Of all trees that are in the wood
    The holly bears the crown

--- src/2020/day04.js
+++ src/2020/day04.js
@@ -1,12 +1,93 @@
 const utils = require('../util/fileUtil');
 
+const requiredAttributes = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+// , 'cid'
+
 const firstStar = function (input) {
-  return 0;
+  let passports = getPassports(input);
+  return passports.filter(passport => hasRequiredAttributes(passport)).length;
 }
 
 const secondStar = function (input) {
-  return 0;
+  let passports = getPassports(input);
+  return passports
+    .filter(passport => hasRequiredAttributes(passport))
+    .filter(passport => hasRequiredValues(passport))
+    .length;
 }
+
+const getPassports = function (input) {
+  let passport = '';
+  let passports = new Array();
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].trim() === '') {
+      passports.push(passport);
+      passport = '';
+    } else {
+      if (passport !== '') {
+        passport += ' ';
+      }
+      passport += input[i];
+    }
+  }
+  passports.push(passport);
+  return passports;
+}
+
+const hasRequiredAttributes = function (passport) {
+  let passportAttr = passport.split(' ')
+    .map(x => x.split(':')[0]);
+  return requiredAttributes.every(x => passportAttr.includes(x));
+}
+
+const validEyeColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+const hasRequiredValues = function (passport) {
+  return passport.split(' ')
+    .every(x => {
+      let key = x.split(':')[0];
+      let value = x.split(':')[1];
+      switch (key) {
+        case 'byr':
+          // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+          let bYear = parseInt(value);
+          return bYear >= 1920 && bYear <= 2002;
+        case 'iyr':
+          // iyr(Issue Year) - four digits; at least 2010 and at most 2020.
+          let iYear = parseInt(value);
+          return iYear >= 2010 && iYear <= 2020;
+        case 'eyr':
+          // eyr(Expiration Year) - four digits; at least 2020 and at most 2030.
+          let eYear = parseInt(value);
+          return eYear >= 2020 && eYear <= 2030;
+        case 'hgt':
+          // hgt(Height) - a number followed by either cm or in:
+          // If cm, the number must be at least 150 and at most 193.
+          // If in, the number must be at least 59 and at most 76.
+          if (value.includes('cm')) {
+            let height = parseInt(value.replace('cm', ''));
+            return height >= 150 && height <= 193;
+          } else {
+            let height = parseInt(value.replace('in', ''));
+            return height >= 59 && height <= 76;
+          }
+        case 'hcl':
+          // hcl(Hair Color) - a # followed by exactly six characters 0 - 9 or a - f.
+          const hclRegex = RegExp('^#[0-9a-f]{6}$');
+          return hclRegex.test(value);
+        case 'ecl':
+          // ecl(Eye Color) - exactly one of: .
+          return validEyeColors.includes(value);
+        case 'pid':
+          // pid(Passport ID) - a nine - digit number, including leading zeroes.
+          const pidRegex = RegExp('^[0-9]{9}$');
+          return pidRegex.test(value);
+        case 'cid':
+          // cid(Country ID) - ignored, missing or not.
+          return true;
+      }
+    });
+}
+
 
 exports.run = function () {
   let input = utils.getInput('2020', 'day04', '\n');
@@ -16,3 +97,4 @@ exports.run = function () {
 
 exports.runOne = firstStar;
 exports.runTwo = secondStar;
+exports.hasRequiredValues = hasRequiredValues;
--- test/2020/day04_test.js
+++ test/2020/day04_test.js
@@ -1,20 +1,39 @@
 const sut = require('../../src/2020/day04');
+const fs = require('fs');
 const assert = require('assert');
 
 describe('2020 - Day 04', function () {
 
   describe('firstStar', function () {
+    let input = fs.readFileSync('./test/data/202004', 'utf8').split('\n');
 
-    it('should ...', function () {
-      assert.strictEqual(sut.runOne(), 0);
+    it('should validate passports attributes', function () {
+      assert.strictEqual(sut.runOne(input), 2);
     });
 
   });
 
   describe('secondStar', function () {
+    let input = fs.readFileSync('./test/data/202004', 'utf8').split('\n');
 
     it('should ...', function () {
-      assert.strictEqual(sut.runTwo(), 0);
+      assert.strictEqual(sut.runTwo(input), 2);
+    });
+
+  });
+
+  describe('hasRequiredValues', function () {
+
+    it('should validate passport attribute values', function () {
+      assert.strictEqual(sut.hasRequiredValues('pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f'), true);
+      assert.strictEqual(sut.hasRequiredValues('eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm'), true);
+      assert.strictEqual(sut.hasRequiredValues('hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022'), true);
+      assert.strictEqual(sut.hasRequiredValues('iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719'), true);
+
+      assert.strictEqual(sut.hasRequiredValues('eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926'), false);
+      assert.strictEqual(sut.hasRequiredValues('iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946'), false);
+      assert.strictEqual(sut.hasRequiredValues('hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277'), false);
+      assert.strictEqual(sut.hasRequiredValues('hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007'), false);
     });
 
   });
