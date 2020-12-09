a45f8bb0fed62b4d778fb52e207ab43ce76ffa15
h0yta <oscar@vettig.se>
2020-12-09 21:31:54 +0100

    Deck the halls with boughs of holly
    Fa la la la la, la la la la

--- test/2015/day03_test.js
+++ test/2015/day03_test.js
@@ -5,15 +5,15 @@ describe('2015 - 03', function () {
 
   describe('firstStar', function () {
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runOne(['>']), 2);
     });
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runOne(['^', '>', 'v', '<']), 4);
     });
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runOne(['^', 'v', '^', 'v', '^', 'v', '^', 'v', '^', 'v']), 2);
     });
 
@@ -21,15 +21,15 @@ describe('2015 - 03', function () {
 
   describe('secondStar', function () {
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runTwo(['^', 'v']), 3);
     });
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runTwo(['^', '>', 'v', '<']), 3);
     });
 
-    it('otototshould calculate number of houses', function () {
+    it('should calculate number of houses', function () {
       assert.strictEqual(sut.runTwo(['^', 'v', '^', 'v', '^', 'v', '^', 'v', '^', 'v']), 11);
     });
 
