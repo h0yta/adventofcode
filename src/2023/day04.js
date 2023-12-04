32b8e5d60a126effb363978668a00a5053e5cdd5
h0yta <oscar@vettig.se>
2023-12-04 07:44:24 +0100

    And checking it twice

--- src/2023/day04.js
+++ src/2023/day04.js
@@ -1,11 +1,46 @@
 const utils = require('../util/fileUtil');
 
 const firstStar = (input) => {
-  return 0;
+  return input
+    .map(parseCard)
+    .map(card => intersect(card.winningNumber, card.lottoryNumber).length)
+    .map(correct => correct > 0 ? Math.pow(2, correct - 1) : 0)
+    .reduce((a, b) => a + b);
 }
 
 const secondStar = (input) => {
-  return 0;
+  let cards = [];
+  input
+    .map(parseCard)
+    .map(card => intersect(card.winningNumber, card.lottoryNumber).length)
+    .reverse()
+    .forEach(point => {
+      if (point > 0) {
+        cards.push(cards.slice(-point).reduce((a, b) => a + b) + 1);
+      } else {
+        cards.push(1);
+      }
+    });
+
+  return cards.reduce((a, b) => a + b);
+}
+
+const intersect = (arr1, arr2) => {
+  return arr1.filter(x => arr2.includes(x))
+}
+
+const parseCard = (row) => {
+  let rowRegex = /^Card\s+(\d+):([\d\s]+)\|([\d\s]+)$/;
+  let match = rowRegex.exec(row.trim());
+  if (match === null) {
+    console.log('Found no match for', row);
+  } else {
+    return {
+      id: parseInt(match[1].trim()),
+      winningNumber: match[2].trim().split(/\s+/),
+      lottoryNumber: match[3].trim().split(/\s+/)
+    }
+  }
 }
 
 exports.run = () => {
--- test/2023/day04_test.js
+++ test/2023/day04_test.js
@@ -7,8 +7,8 @@ describe('2023 - 04', () => {
   describe('firstStar', () => {
     let input = fs.readFileSync('./test/data/2023/day04-1', 'utf8').split('\n');
 
-    it('ototot should ...', () => {
-      assert.strictEqual(sut.runOne(input), 0);
+    it(' should summarize all the card points', () => {
+      assert.strictEqual(sut.runOne(input), 13);
     });
 
   });
@@ -16,8 +16,8 @@ describe('2023 - 04', () => {
   describe('secondStar', () => {
     let input = fs.readFileSync('./test/data/2023/day04-2', 'utf8').split('\n');
 
-    it(' should ...', () => {
-      assert.strictEqual(sut.runTwo(input), 0);
+    it(' should summarize number of card copies', () => {
+      assert.strictEqual(sut.runTwo(input), 30);
     });
 
   });
