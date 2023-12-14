40f41da2d15828d2ad94f8645b01bf1ecb9b02f7
h0yta <oscar@vettig.se>
2023-12-14 21:08:42 +0100

    Let it snow, let it snow, let it snow

---
+++ test/2023/day14_test.js
@@ -0,0 +1,26 @@
+const sut = require('../../src/2023/day14');
+const fs = require('fs');
+const assert = require('assert');
+
+describe('2023 - 14', () => {
+
+  describe('firstStar', () => {
+    let input = fs.readFileSync('./test/data/2023/day14-1', 'utf8').split('\n');
+
+    it('ototot should ...', () => {
+      assert.strictEqual(sut.runOne(input), 136);
+    });
+
+  });
+
+  describe('secondStar', () => {
+    let input = fs.readFileSync('./test/data/2023/day14-2', 'utf8').split('\n');
+
+    it(' should ...', () => {
+      assert.strictEqual(sut.runTwo(input), 0);
+    });
+
+  });
+
+});
+
